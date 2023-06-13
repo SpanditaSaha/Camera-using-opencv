91f4cc233445efcb6c329225bc14c68c7ba6a436
Spandita Saha <67426987+SpanditaSaha@users.noreply.github.com>
2023-06-13 19:20:14 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,26 +1,9 @@
-# import cv2
-
-# cam = cv2.VideoCapture(0)
-
-# while True:
-#     check, frame = cam.read()
-
-#     cv2.imshow('video', frame)
-
-#     key = cv2.waitKey(1)
-#     if key == 27:
-#         break
-
-# cam.release()
-# cv2.destroyAllWindows()
-
-
 # import the opencv library
 import cv2
 
 
 # define a video capture object
-vid = cv2.VideoCapture(-1)
+vid = cv2.VideoCapture(0)
 
 while(True):
 	
